c8f8bb462b127ddc4daed33f956c4f58049ab24f
Maxwell Almeida <46318799+maxwellpalmeida@users.noreply.github.com>
2019-03-21 19:07:12 -0300

    Adjustments to enable use on multiple pages of same domain

--- ScrollBehavior.js
+++ ScrollBehavior.js
@@ -1,9 +1,4 @@
-function tryit() {
-    alert("this is a test");
-}
-
 window.addEventListener('scroll', function (e) {
-    debugger;
     var last_known_scroll_position = window.scrollY;
     window.requestAnimationFrame(function () {
         StoreScrollPosition();
@@ -11,23 +6,77 @@ window.addEventListener('scroll', function (e) {
     });
 });
 
-function ValidadeBrowser() {
-    if (typeof (Storage) == "undefined") {
-        alert("Sorry, your browser is not ready to keep all our functionality");
-        return false;
+function objectUnique(array, field, value) {
+    var objectFound = undefined;
+    if (array.length > 0) {
+        array.forEach(function (entry) {
+            if (entry[field] == value) {
+                objectFound = entry;
+            }
+        });
     }
+    return objectFound;
+}
+
+function ScrollFunctionality() {
+    o = this;
+    o.urlPositionsStored = [];
+    o.pulledScrollObject;
+    o.current = {
+        url: window.location.href,
+        scroll: window.scrollY
+    };
+    o.ValidadeBrowser = function () {
+        if (typeof (Storage) == "undefined") {
+            alert("Sorry, your browser is not ready to keep all our functionality");
+            return false;
+        }
+    }
+    o.pullObjectFromArray = function () {
+        var objectFound = undefined;
+        if (o.urlPositionsStored.length > 0) {
+            o.urlPositionsStored.forEach(function (entry) {
+                if (entry["url"] == o.current.url) {
+                    objectFound = entry;
+                }
+            });
+        }
+        return objectFound;
+    }
+    o.Inicialize = function () {
+        o.ValidadeBrowser();
+
+        if (localStorage.getItem("urlPositions") != undefined) {
+            o.urlPositionsStored = JSON.parse(localStorage.getItem("urlPositions"));
+        }
+
+        o.pulledScrollObject = o.pullObjectFromArray(o.current.url);
+    }
+
+    o.Inicialize();
 }
 
 function StoreScrollPosition() {
-    ValidadeBrowser();
-    localStorage.setItem("urlPositions", window.scrollY);
+    var ScrollFuncs = new ScrollFunctionality();
+
+    if (ScrollFuncs.pulledScrollObject != undefined) {
+        ScrollFuncs.pulledScrollObject.scroll = ScrollFuncs.current.scroll;
+    }
+    else {
+        ScrollFuncs.urlPositionsStored.push({
+            scroll: ScrollFuncs.current.scroll,
+            url: ScrollFuncs.current.url,
+            date: new Date(),
+        });
+    }
+    localStorage.setItem("urlPositions", JSON.stringify(ScrollFuncs.urlPositionsStored));
 }
 
 function RecoverScrollPosition() {
-    ValidadeBrowser();
+    var ScrollFuncs = new ScrollFunctionality();
     if (localStorage.getItem("urlPositions") != undefined) {
-     //   document.documentElement.scrollTop = ;
-        window.scrollTo(0, localStorage.getItem("urlPositions"));
+        if (ScrollFuncs.pulledScrollObject != undefined)
+            window.scrollTo(0, ScrollFuncs.pulledScrollObject.scroll);
     }
 }
 
